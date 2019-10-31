05fb30d5046f3a68c7978af0b67c0cf7a89a8262
Renée <42935816+ReneeR33@users.noreply.github.com>
2019-10-31 10:14:52 +0100

    modes in modules gezet

---
+++ Arduino_code/mode_addfingerprint.ino
@@ -0,0 +1,47 @@
+void addFingerprint(){
+  if (fingerPrintScan == FINGERPRINT_OK && fingerPrintScanProgress < 3) {
+      fingerPrintScan = finger.image2Tz(fingerPrintScanProgress);
+      if (fingerPrintScan == FINGERPRINT_OK) {
+        fingerPrintScanProgress++;
+        Serial.println("OK");
+        Serial.println(fingerPrintScanProgress);
+        delay(500);
+      }
+    }
+    if (fingerPrintScanProgress == 3) {
+      Serial.println("2 Images taken");
+      fingerPrintScan = finger.createModel();
+      if (fingerPrintScan == FINGERPRINT_OK) {
+        Serial.println("Created a model");
+        // HIER
+        int id = GetFirstFreeSpace();
+        //
+        if (id != -1) {
+          fingerPrintScan = finger.storeModel(id);
+          if (fingerPrintScan == FINGERPRINT_OK) {
+            Serial.print("Stored, id value = ");
+            Serial.println(id);
+          }
+        }
+        else {
+          Serial.println("Scanner is full"
+
+      }
+      fingerPrintScanProgress = 1;
+    }
+    if (buttonPin_2_Value == HIGH) {
+      mode = "OPENLOCK";
+      Serial.println(mode););
+        }
+      readFingerprint = true;
+      scanFingerprint = false;
+      delay(500);
+    }
+    if (buttonPin_1_Value == HIGH) {
+      mode = "DELETE_FINGERPRINT";
+      Serial.println(mode);
+      readFingerprint = false;
+      scanFingerprint = false;
+      delay(500);
+    }
+}
