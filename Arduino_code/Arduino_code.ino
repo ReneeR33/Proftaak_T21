91145092dad8bf55fda911b0e2e2918c734451bc
Renée <42935816+ReneeR33@users.noreply.github.com>
2019-10-31 11:03:41 +0100

    Revert "modes in modules gezet"
    This reverts commit 05fb30d5046f3a68c7978af0b67c0cf7a89a8262.

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -2,21 +2,6 @@
 #include <Adafruit_Fingerprint.h>
 Servo servo;
 
-//--FINGERPRINTSCANNER--
-//---------------
-// BLACK - GND
-// RED - 5V
-// GREEN - 2
-// WHITE - 3
-//---------------
-
-//--SERVO--
-//---------------
-// BROWN - GND
-// RED - 5V
-// YELLOW - 9
-//---------------
-
 SoftwareSerial mySerial(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
@@ -64,16 +49,104 @@ void loop() {
     fingerPrintScan = finger.getImage();
   }
 
+
+  //MODE: OPENLOCK
+
   if (mode == "OPENLOCK") {
-    openLock();
+
+    if (buttonPin_1_Value == HIGH && isOpen == true) {
+      servo.write(0);
+      isOpen = false;
+      endTimeServo = millis() + 500;
+    }
+
+    if (fingerID > 0 && isOpen == false) {
+      servo.write(180);
+      isOpen = true;
+      endTimeServo = millis() + 500;
+    }
+
+    if (millis() > endTimeServo) {
+      servo.write(90);
+    }
+
+    if (buttonPin_2_Value == HIGH) {
+      mode = "ADDFINGERPRINT";
+      Serial.println(mode);
+      readFingerprint = false;
+      scanFingerprint = true;
+      switchMode = true;
+      delay(500);
+    }
   }
+
+  //MODE: ADDFINGERPRINT
 
   else if (mode == "ADDFINGERPRINT") {
-    addFingerprint();
+    if (fingerPrintScan == FINGERPRINT_OK && fingerPrintScanProgress < 3) {
+      fingerPrintScan = finger.image2Tz(fingerPrintScanProgress);
+      if (fingerPrintScan == FINGERPRINT_OK) {
+        fingerPrintScanProgress++;
+        Serial.println("OK");
+        Serial.println(fingerPrintScanProgress);
+        delay(500);
+      }
+    }
+    if (fingerPrintScanProgress == 3) {
+      Serial.println("2 Images taken");
+      fingerPrintScan = finger.createModel();
+      if (fingerPrintScan == FINGERPRINT_OK) {
+        Serial.println("Created a model");
+        // HIER
+        int id = GetFirstFreeSpace();
+        //
+        if (id != -1) {
+          fingerPrintScan = finger.storeModel(id);
+          if (fingerPrintScan == FINGERPRINT_OK) {
+            Serial.print("Stored, id value = ");
+            Serial.println(id);
+          }
+        }
+        else{
+          Serial.println("Scanner is full");
+        }
+
+      }
+      fingerPrintScanProgress = 1;
+    }
+    if (buttonPin_2_Value == HIGH) {
+      mode = "OPENLOCK";
+      Serial.println(mode);
+      readFingerprint = true;
+      scanFingerprint = false;
+      delay(500);
+    }
+    if (buttonPin_1_Value == HIGH) {
+      mode = "DELETE_FINGERPRINT";
+      Serial.println(mode);
+      readFingerprint = false;
+      scanFingerprint = false;
+      delay(500);
+    }
   }
 
-  else if (mode == "DELETE_FINGERPRINT") {
-    deleteFingerprint();
+  else if(mode == "DELETE_FINGERPRINT"){
+
+    if(message.startsWith("DELETE:")){
+      message.remove(0,7);
+      int id = message.toInt();
+      if(RemoveFingerprint(id)){
+        Serial.print("Deleted fingeprint with id = ");
+        Serial.println(id);
+       }
+    }
+    if (buttonPin_2_Value == HIGH) {
+      mode = "OPENLOCK";
+      Serial.println(mode);
+      readFingerprint = true;
+      scanFingerprint = false;
+      delay(500);
+    }
   }
 
 
