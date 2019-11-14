2f9515ca12b78200784fd160720b57e1c3597c41
FaasBoers <56442684+FaasBoers@users.noreply.github.com>
2019-11-14 16:07:21 +0100

    FaasBoers
    GoogleMaps

---
+++ PC_code/SmartBike/SmartBike/FormLocation.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace SmartBike
+{
+    partial class FormLocation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(12, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(601, 426);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            // 
+            // FormLocation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 450);
+            this.Controls.Add(this.map);
+            this.Name = "FormLocation";
+            this.Text = "FormLocation";
+            this.Load += new System.EventHandler(this.FormLocation_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+    }
+}
---
+++ PC_code/SmartBike/SmartBike/FormLocation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace SmartBike
+{
+    public partial class FormLocation : Form
+    {
+        public FormLocation()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLocation_Load(object sender, EventArgs e)
+        {
+            double lat = 52;
+            double lng = 6;
+
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.Position = new PointLatLng(lat, lng);
+            map.MinZoom = 5;
+            map.MaxZoom = 20;
+            map.Zoom = 15;
+
+            PointLatLng point = new PointLatLng(lat, lng);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red_dot);
+
+            GMapOverlay markers = new GMapOverlay("markers");
+            markers.Markers.Add(marker);
+            map.Overlays.Add(markers);
+        }
+    }
+}
