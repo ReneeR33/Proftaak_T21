3e088f495b1fd9a9171c9dd94fad2aba6ee77e5a
Renée <42935816+ReneeR33@users.noreply.github.com>
2020-01-09 11:22:39 +0100

    Code aangepast, keypad verwijderd

--- Arduino_code/mode_addfingerprint.ino
+++ Arduino_code/mode_addfingerprint.ino
@@ -1,49 +1,55 @@
 int fingerPrintScan = FINGERPRINT_INVALIDIMAGE;
 int fingerPrintScanProgress = 1;
 
-void addFingerprintK() {
-  //fingerPrintScan = finger.getImage();
-  char customKey = customKeypad.getKey();
+/*void addFingerprintK() {
+  fingerPrintScan = finger.getImage();
+  //char customKey = customKeypad.getKey();
   //int key = (int)customKey - 48;
-  
-  if (customKey && fingerPrintScanProgress < 3) {
-    //fingerPrintScan = finger.image2Tz(fingerPrintScanProgress);
-    
-    fingerPrintScanProgress++;
-    Serial.println("OK");
-    if(fingerPrintScanProgress == 2) Serial.print("#FIRST_SCANNED%");
-    else if (fingerPrintScanProgress == 3) Serial.print("#SECOND_SCANNED%");
-    Serial.println(fingerPrintScanProgress);
-    delay(500);
-  }
-  if (fingerPrintScanProgress == 3) {
-    Serial.println("2 Images taken");
-    //fingerPrintScan = finger.createModel();
+  Serial.println(fingerPrintScan);
 
-    Serial.println("Created a model");
-
-    int id = GetFirstFreeSpace();
-
-    if (id != -1) {
-      EEPROM.put(id, 1);
-      Serial.print("Stored, id value = ");
-      Serial.println(id);
-      Serial.print("#ADDED_FINGERPRINT:");
-      Serial.print(id);
-      Serial.print("%");
+  if (fingerPrintScan == FINGERPRINT_OK) {
+    if (fingerPrintScanProgress < 3) {
+      fingerPrintScan = finger.image2Tz(fingerPrintScanProgress);
+      fingerPrintScanProgress++;
+      Serial.println("OK");
+      if (fingerPrintScanProgress == 2) Serial.print("#FIRST_SCANNED%");
+      else if (fingerPrintScanProgress == 3) Serial.print("#SECOND_SCANNED%");
+      Serial.println(fingerPrintScanProgress);
+      delay(500);
     }
-    fingerPrintScanProgress = 1;
-  }
-}
+    if (fingerPrintScanProgress == 3) {
+      Serial.println("2 Images taken");
+      fingerPrintScan = finger.createModel();
 
-/*void addFingerprint() {
+      Serial.println("Created a model");
+
+      int id = GetFirstFreeSpace();
+
+      if (id > 0) {
+        EEPROM.put(id, 2);
+        Serial.print("Stored, id value = ");
+        Serial.println(id);
+        Serial.print("#ADDED_FINGERPRINT:");
+        Serial.print(id);
+        Serial.print("%");
+      }
+      fingerPrintScanProgress = 1;
+    }
+  }
+
+}*/
+
+void addFingerprint() {
   fingerPrintScan = finger.getImage();
   if (fingerPrintScan == FINGERPRINT_OK && fingerPrintScanProgress < 3) {
     fingerPrintScan = finger.image2Tz(fingerPrintScanProgress);
     if (fingerPrintScan == FINGERPRINT_OK) {
       fingerPrintScanProgress++;
       Serial.println("OK");
-      Serial.println(fingerPrintScanProgress);
+      if (fingerPrintScanProgress == 2) Serial.print("#FIRST_SCANNED%");
+      else if (fingerPrintScanProgress == 3) Serial.print("#SECOND_SCANNED%");
+      
+      //Serial.println(fingerPrintScanProgress);
       delay(500);
     }
   }
@@ -58,8 +64,12 @@ void addFingerprintK() {
       if (id != -1) {
         fingerPrintScan = finger.storeModel(id);
         if (fingerPrintScan == FINGERPRINT_OK) {
+          EEPROM.put(id, 2);
           Serial.print("Stored, id value = ");
           Serial.println(id);
+          Serial.print("#ADDED_FINGERPRINT:");
+          Serial.print(id);
+          Serial.print("%");
         }
       }
       else {
@@ -70,4 +80,3 @@ void addFingerprintK() {
     fingerPrintScanProgress = 1;
   }
 }
-*/
